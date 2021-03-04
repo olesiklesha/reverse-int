d900fe4ab0f882873593639c1f3c4b42f365ef01
Alex Olesik <olesik736@gmail.com>
2021-03-04 20:39:08 +0300

    feat: write function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n < 0) return Number(String(n * (-1)).split('').reverse().join(''))
+	return Number(String(n).split('').reverse().join(''))
 }
